3112e9f5ebbd2dd40eab5daca281944c7e0897a9
jblue6 <jason.blue6@gmail.com>
2019-11-28 17:07:25 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const config = require("config");
+
+const app = express();
+
+app.use(express.json());
+
+const db = config.get("mongoURI");
+
+//connect to Mongo
+mongoose
+  .connect(db, { useNewUrlParser: true, useCreateIndex: true })
+  .then(() => console.log("Mongo DB connected"))
+  .catch(err => console.log(err));
+
+// use routes
+app.use("/api/data", require("./routes/data"));
+
+// serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
